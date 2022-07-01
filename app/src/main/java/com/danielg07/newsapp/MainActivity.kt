ae3608835c2ff843f2221b3c9a495e9033f4bc2e
Daniel Gonzalez <enda0507@gmail.com>
2022-07-01 02:41:38 -0500

    Validación de permiso de localización
    Se hace manejo de permisos para localización, ademas de mostrar mensajes (Snackbar) para saber si se hace un control correctamente.

--- app/src/main/java/com/danielg07/newsapp/MainActivity.kt
+++ app/src/main/java/com/danielg07/newsapp/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.danielg07.newsapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.danielg07.newsapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            verificarPermisos()
+        }
     }
+
+    private fun verificarPermisos() {
+        val location = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (location == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(findViewById(R.id.error),
+            "Mostrando noticias",
+            Snackbar.LENGTH_LONG).show()
+        }
+        else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Snackbar.make(findViewById(R.id.error),
+                    "Granted",
+                    Snackbar.LENGTH_LONG).show()
+            } else {
+                binding.error.visibility = View.VISIBLE
+                binding.button.visibility = View.GONE
+            }
+        }
 }

594d13af368b78e29ea752a0996d232b7224a66d
Daniel Gonzalez <enda0507@gmail.com>
2022-07-03 23:31:21 -0500

    Asigna un valor predeterminado al modelo
    Se le asigno una cadena vacia como valor predeterminado en caso que nos encontremos con un valor nulo como respuesta y evitar errores

--- app/src/main/java/com/danielg07/newsapp/model/Article.kt
+++ app/src/main/java/com/danielg07/newsapp/model/Article.kt
@@ -2,11 +2,11 @@ package com.danielg07.newsapp.model
 
 data class Article(
     val source: Source,
-    val author: String,
-    val title: String,
-    val description: String,
-    val url: String,
-    val urlToImage: String,
-    val publishedAt: String,
-    val content: String
+    val author: String? = "",
+    val title: String? = "",
+    val description: String? = "",
+    val url: String? = "",
+    val urlToImage: String? = "",
+    val publishedAt: String? = "",
+    val content: String? = ""
 )
--- app/src/main/java/com/danielg07/newsapp/model/Source.kt
+++ app/src/main/java/com/danielg07/newsapp/model/Source.kt
@@ -2,5 +2,5 @@ package com.danielg07.newsapp.model
 
 data class Source(
     val id: String? = "",
-    val name: String
+    val name: String? = ""
 )

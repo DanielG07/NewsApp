d47ad480e918b17307db6afd08e33a1a926320f8
Daniel Gonzalez <enda0507@gmail.com>
2022-07-04 14:09:20 -0500

    Se crean y configura para navegación hacia la vista de detalles
    Se modifican los modelos para hacer uso de Parcelize y se pueda pasar por el nav_graph. Ademas se crea un grafico para la navegación "navigation.xml", donde en el "activy_news" se hace la configuración para lograr implementar el nav_graph. Se crea el fragment para los detalles ademas de que se colocan sus elementos donde se va a mostrar la información. En el ViewHolder para las noticias se implementa la navegación para ir al fragment de los detalles.

--- app/src/main/java/com/danielg07/newsapp/model/Source.kt
+++ app/src/main/java/com/danielg07/newsapp/model/Source.kt
@@ -1,6 +1,10 @@
 package com.danielg07.newsapp.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Source(
     val id: String? = "",
     val name: String? = ""
-)
+) : Parcelable
--- app/src/main/java/com/danielg07/newsapp/news/NewViewHolder.kt
+++ app/src/main/java/com/danielg07/newsapp/news/NewViewHolder.kt
@@ -1,12 +1,22 @@
 package com.danielg07.newsapp.news
 
+import android.util.Log
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.danielg07.newsapp.databinding.ListViewItemBinding
 import com.danielg07.newsapp.model.Article
+import com.danielg07.newsapp.tablayout.MainFragmentDirections
 
 class NewViewHolder(private var binding: ListViewItemBinding): RecyclerView.ViewHolder(binding.root) {
+
     fun bind(ItemResult: Article){
         binding.article = ItemResult
+        binding.btnMore.setOnClickListener{
+            Log.d("Boton", "Quiero ver mas ${ItemResult.title}")
+
+            val action = MainFragmentDirections.actionMainFragment2ToDetailFragment(ItemResult)
+            Navigation.findNavController(binding.root).navigate(action)
+        }
         binding.executePendingBindings()
     }
 }

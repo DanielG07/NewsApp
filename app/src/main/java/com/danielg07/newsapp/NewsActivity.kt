6b2514b3b85a099aef0ea58321fd9e68c928ec51
Daniel Gonzalez <enda0507@gmail.com>
2022-07-03 23:34:40 -0500

    Se remueve codigo que no se usa
    Se remueve codigo que se uso para hacer las primeras peticiones y probar que funcionara correctamente las peticiones hacia la API.

--- app/src/main/java/com/danielg07/newsapp/NewsActivity.kt
+++ app/src/main/java/com/danielg07/newsapp/NewsActivity.kt
@@ -17,21 +17,6 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //getNews()
     }
 
-    // Se hace la prueba para el consumo de la API - Ejemplo
-    // NO IMPLEMENTAR
-    private fun getNews() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val total = NewsApi.retrofitService.getNews("futbol")
-            Snackbar.make(binding.root,
-                "Resultados: ${total.totalResults}",
-                Snackbar.LENGTH_LONG).show()
-            runOnUiThread {
-                //binding.textView.text = total.articles.toString()
-            }
-
-        }
-    }
 }
